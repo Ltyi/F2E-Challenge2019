3216f69faa3c637cd3920f2c27a8d491bd4aae72
Ryan <ltyi.box@gmail.com>
2020-11-30 17:36:03 +0800

    +week2 and demo page

--- week1/.eslintrc.js
+++ week1/.eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/vue3-recommended",
-    "@vue/standard",
-    "@vue/prettier",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-recommended',
+    '@vue/standard',
+    '@vue/prettier',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
-  plugins: ["prettier"],
+  plugins: ['prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
