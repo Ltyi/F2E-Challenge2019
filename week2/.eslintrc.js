e3e60429d3b3efdcf3b4d4fb1348de201fd5d8b7
Ryan <ltyi.box@gmail.com>
2020-12-07 16:00:35 +0800

    eslint setting

--- week2/.eslintrc.js
+++ week2/.eslintrc.js
@@ -3,15 +3,23 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/vue3-recommended", "@vue/standard"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "space-before-function-paren": ["error", "never"],
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: 2,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
-}
+};
