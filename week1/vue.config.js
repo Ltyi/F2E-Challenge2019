ca8276602aaa7967de2a7becb37b0870393a1c16
Ryan <ltyi.box@gmail.com>
2020-12-07 10:57:22 +0800

    vue.config.js

--- week1/vue.config.js
+++ week1/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production' ? '/week1/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/F2E-Challenge2019/week1/' : '/',
   parallel: false,
   configureWebpack: {
     externals: {
